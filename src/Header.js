4a5a12efc71641908a3b20d707ab70db04bec339
Allan Ched <allanched113@gmail.com>
2024-01-03 13:50:29 -0300

    alteracao no alinhamento do link

--- src/Header.js
+++ src/Header.js
@@ -9,9 +9,9 @@ function Header() {
             <div id="header-content">
                 <h1>Allan Ched</h1>
                 <ul id="links">
-                    <li className='link'><a href="https://wa.me/5551995526209"><img alt="logo" src={whatsappImg}></img>WhatsApp</a></li>
-                    <li className='link'><a href="https://www.linkedin.com/in/allan-ched-lucas-rodrigues-57225822a/"><img alt="logo" src={linkedinImg}></img>LinkedIn</a></li>
-                    <li className='link'><a href="https://github.com/AllanChed"><img alt="logo" src={githubImg}></img>GitHub</a></li>
+                    <li className='link'><a href="https://wa.me/5551995526209"><img alt="logo" src={whatsappImg}></img><span id='text-position'>WhatsApp</span></a></li>
+                    <li className='link'><a href="https://www.linkedin.com/in/allan-ched-lucas-rodrigues-57225822a/"><img alt="logo" src={linkedinImg}></img><span id='text-position'>LinkedIn</span></a></li>
+                    <li className='link'><a href="https://github.com/AllanChed"><img alt="logo" src={githubImg}></img><span id='text-position'>GitHub</span></a></li>
                 </ul>
             </div>
         </div>
